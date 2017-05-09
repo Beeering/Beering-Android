4a514a04265ec2cbc71e58c8c6240a39170513a0
hyesun03 <chsun0303@gmail.com>
2017-05-09 15:46:33 +0900

    아 왜 자꾸 앱이 죽지?

--- Beering/app/src/main/java/io/beering/beering/MainActivity.java
+++ Beering/app/src/main/java/io/beering/beering/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
@@ -13,11 +14,21 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import io.beering.beering.Fragement.BeerFragment;
+import io.beering.beering.Fragement.ProfileFragment;
+import io.beering.beering.Fragement.PubFragment;
+
 import static io.beering.beering.R.id.logo_text;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
+    private ViewPager viewPager;
+
+    BeerFragment beerFragment;
+    ProfileFragment profileFragment;
+    PubFragment pubFragment;
+    MenuItem prevMenuItem;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -26,12 +37,15 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.main_beer:
+                    viewPager.setCurrentItem(0);
                     mTextMessage.setText(R.string.main_beer);
                     return true;
                 case R.id.main_pub:
+                    viewPager.setCurrentItem(1);
                     mTextMessage.setText(R.string.main_pub);
                     return true;
                 case R.id.my_profile:
+                    viewPager.setCurrentItem(2);
                     mTextMessage.setText(R.string.my_profile);
                     return true;
             }
@@ -40,14 +54,18 @@ public class MainActivity extends AppCompatActivity {
 
     };
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        viewPager = (ViewPager)findViewById(R.id.viewpager);
+
+        final BottomNavigationView bottomNavigation = (BottomNavigationView) findViewById(R.id.navigation);
+        bottomNavigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
         if (Build.VERSION.SDK_INT >= 21) {
             getWindow().setStatusBarColor(Color.rgb(220, 90, 1));
@@ -76,6 +94,44 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                if(prevMenuItem != null) {
+                    prevMenuItem.setChecked(false);
+                }
+                else {
+                    bottomNavigation.getMenu().getItem(0).setChecked(false);
+                }
+
+                bottomNavigation.getMenu().getItem(position).setChecked(true);
+                prevMenuItem = bottomNavigation.getMenu().getItem(position);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+
+        setupViewPager(viewPager);
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        beerFragment = new BeerFragment();
+        profileFragment = new ProfileFragment();
+        pubFragment = new PubFragment();
+        adapter.addFragment(beerFragment);
+        adapter.addFragment(profileFragment);
+        adapter.addFragment(pubFragment);
+        viewPager.setAdapter(adapter);
     }
 
 }
