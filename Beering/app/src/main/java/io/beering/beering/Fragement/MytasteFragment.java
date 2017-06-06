aa665d93505083aba9e1ab00bc6b40721cb033a2
hyesun03 <chsun0303@gmail.com>
2017-06-07 07:45:55 +0900

    Change to scroll view

--- Beering/app/src/main/java/io/beering/beering/Fragement/MytasteFragment.java
+++ Beering/app/src/main/java/io/beering/beering/Fragement/MytasteFragment.java
@@ -4,7 +4,6 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -63,8 +62,6 @@ public class MytasteFragment extends Fragment {
     }
 
     private void addDataSet() {
-        Log.d("!!!!!!!!!!", "addDataSet: WHY....");
-
         ArrayList<PieEntry> yEntrys = new ArrayList<>();
         ArrayList<String> xEntrys = new ArrayList<>();
 
